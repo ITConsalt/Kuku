ab7647e7ccdcb6fc85af7dc5e4e905a0c10ba7ba
Alyaksei Yakubouski <aleksei03@mail.ru>
2018-06-22 14:11:45 +0300

    Account/Register do not work

---
+++ Kuku/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kuku.ViewModels;
+using Kuku.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CustomIdentityApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<KukuUser> _userManager;
+        private readonly SignInManager<KukuUser> _signInManager;
+
+        public AccountController(UserManager<KukuUser> userManager, SignInManager<KukuUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                KukuUser user = new KukuUser { UserLogin = model.UserLogin, UserName = model.Name, Surname = model.Surname, Patronymic = model.Patronymic };
+                // add user
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // installation of cookies
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Kuku/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kuku.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string UserLogin { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string PasswordConfirm { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Patronymic")]
+        public string Patronymic { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
