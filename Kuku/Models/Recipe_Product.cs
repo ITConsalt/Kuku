a030b1089dbe68dc5582ec4589c5ed4c1c6bb61f
Alyaksei Yakubouski <aleksei03@mail.ru>
2018-07-22 22:48:18 +0300

    product id to recipe ready, whitout product name

--- Kuku/Models/Recipe_Product.cs
+++ Kuku/Models/Recipe_Product.cs
@@ -1,15 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kuku.Models
 {
     public class Recipe_Product
-    { 
+    {
         //public int Id { get; set; }              //not use, primary key required
+        //[Key, ForeignKey("FK_Recipe_Products_Recipes_RecipeId")]
+        //[Column("RecipeId", Order = 1)]
         public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+        //[Key, ForeignKey("FK_Recipe_Products_Products_ProductId")]
+        //[Column("ProductId", Order = 0)]
         [Key]
         public int ProductId { get; set; }
-        //public string ProductName { get; set; }
         //public Product Product { get; set; }
-        public Recipe Recipe { get; set; }
+        //public string ProductName { get; set; }
     }
 }
