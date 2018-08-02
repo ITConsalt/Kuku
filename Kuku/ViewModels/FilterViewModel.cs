f16657edab1246870f2dad9463ff9fa4df494384
Alyaksei Yakubouski <aleksei03@mail.ru>
2018-08-02 22:11:26 +0300

    Filter v0.2

---
+++ Kuku/ViewModels/FilterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Kuku.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kuku.ViewModels
+{
+    public class FilterViewModel
+    {
+        public Recipe Recipes { get; set; }
+        public IEnumerable<RecipeDetail> RecipesDetails { get; set; }
+
+        public IEnumerable<Recipe_Product> Recipe_Products { get; set; }
+        public List<Product> Products { get; set; }
+
+        public IEnumerable<Recipe_TypeOfDish> Recipe_TypeOfDishes { get; set; }
+        public List<TypeOfDish> TypeOfDishes { get; set; }
+
+        public IEnumerable<Recipe_NationalCuisine> Recipe_NationalCuisenes { get; set; }
+        public List<NationalCuisine> NationalCuisines { get; set; }
+
+        //public List<MeasuringSystem> MeasuringSystems { get; set; }
+    }
+}
