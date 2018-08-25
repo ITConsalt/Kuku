6f18b73e976d25ed72e532d9920f36d8bb7f04a5
Mihey <mihey@hotmail.com>
2018-08-25 13:27:09 +0300

    Фильтр формирование v1

---
+++ Kuku/Models/Recipe_Filter.cs
@@ -0,0 +1,22 @@
+﻿namespace Kuku.Models
+{
+    public class Recipe_Filter
+    {
+        public int itemId { get; set; }
+        public string itemName { get; set; }
+        public string itemType { get; set; }
+        public int itemCount { get; set; }
+        public int itemSort { get; set; }
+
+        /*
+        public string itemType { get; set; }
+        public int itemSort { get; set; }
+        public Filter[] items { get; set; }
+        */
+        Recipe_Filter(Filter item)
+        {
+            this.itemId = item.itemId;
+        }
+  
+    }
+}
