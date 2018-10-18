8b9b4c9fd657964f7e8ff0639901fa613a2b9375
Bluetooth Dmitry <dmitry.bluetooth@gmail.com>
2018-10-18 13:13:37 +0300

    update netcore 2.0.9

--- Kuku/Models/Product.cs
+++ Kuku/Models/Product.cs
@@ -10,6 +10,8 @@ namespace Kuku.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int ProductTypeId { get; set; }
+        public int MeasuringSystemId { get; set; }
+
         public ProductType ProductType { get; set; }//for adding products to recipe
         public MeasuringSystem MeasuringSystem { get; set; }
 
