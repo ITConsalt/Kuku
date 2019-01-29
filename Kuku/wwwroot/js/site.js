0ffc908f4177faeaa7107bc3796a86287d8d6009
Mihey <mihey@hotmail.com>
2019-01-29 17:21:08 +0300

    Обновление фильтра

--- Kuku/wwwroot/js/site.js
+++ Kuku/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $('a.show-more').on('click', function () {
+        $($(this).data('target')).dropdown('toggle');
+        return false;
+    });
+});
