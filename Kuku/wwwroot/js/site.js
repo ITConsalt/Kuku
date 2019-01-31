2eefa54fedd365cd4d93a3d21bf1d4ed5b136b52
Mihey <mihey@hotmail.com>
2019-01-31 17:11:44 +0300

    Переход по ссылке из выпада.ющего меню в фильтре

--- Kuku/wwwroot/js/site.js
+++ Kuku/wwwroot/js/site.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
         $($(this).data('target')).dropdown('toggle');
         return false;
     });
+    $('.filter-product .dropdown-menu').on('show.bs.dropdown', function () { $(this).off('click') });
+    $('.filter-product .dropdown-menu a.dropdown-item').on('click', function () {
+        window.location = $(this).attr('href');
+    });
 });
